c20609edd7388bb5c06b6b754d272b013877626b
enerzilius <efblouback@gmail.com>
2025-08-23 17:03:30 -0300

    começando fog animada

--- Scripts/draw_terrain.gd
+++ Scripts/draw_terrain.gd
@@ -109,6 +109,8 @@ var cam_angle: Vector3
 
 var common: Common = Common.new()
 
+#var start_time: float = Time.get_unix_time_from_system()
+
 func _init():
 	effect_callback_type = CompositorEffect.EFFECT_CALLBACK_TYPE_POST_TRANSPARENT
 	
@@ -359,6 +361,10 @@ func _render_callback(_effect_callback_type : int, render_data : RenderData):
 	buffer.push_back(max_fog_dist)
 	buffer.push_back(fog_density)
 	buffer.push_back(layered_top)
+	
+	#var current_time: float = (Time.get_unix_time_from_system() - start_time)/1000
+	#var fog_time: float = 0.000035 * (randf() / 10) * current_time
+	#buffer.push_back(fog_time)
 	
 	if not cam:
 		var tree := Engine.get_main_loop() as SceneTree
